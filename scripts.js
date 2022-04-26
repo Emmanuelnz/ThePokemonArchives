e05336ea645bd5e51184e96435ae2c9552cbd9db
Emmanuelnz <emmanuelnun75@gmail.com>
2022-04-26 15:42:25 -0400

    Removed old Code and added array for Pokémon list

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-alert('Hello World');
-let myName='Emmanuel';
-document.write(myName);
-myName='John Doe';
-document.write(myName);
+let pokemonList = [
+  {name:'Umbreon', height:1, types:'Dark'},
+  {name:'Zekrom', height:2.9, types:'Dragon, Electric'},
+  {name:'Luxray', height:1.4, types:'Electric'},
+];
