b4a3fd9a16394ad91c61d4a7683b1f929ee0838a
Emmanuelnz <emmanuelnun75@gmail.com>
2022-04-27 12:42:50 -0400

    changed code format for easier readability

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-  {name:'Umbreon', height:1, types:'Dark'},
-  {name:'Zekrom', height:2.9, types:'Dragon, Electric'},
-  {name:'Luxray', height:1.4, types:'Electric'},
+  {
+    name: 'Umbreon',
+    height: 1,
+    types: ['Dark']
+  },
+  {name: 'Zekrom',
+  height: 2.9,
+  types: ['Dragon, Electric']
+  },
+  {name: 'Luxray',
+  height: 1.4,
+  types: ['Electric']
+  }
 ];
