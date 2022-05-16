7eed934ec888c5359b116d077ec19b2204663cd7
Emmanuelnz <emmanuelnun75@gmail.com>
2022-05-16 02:11:17 -0400

    changed "for" loop to a "forEach" loop

--- scripts.js
+++ scripts.js
@@ -15,14 +15,8 @@ let pokemonList = [
     types: ['Electric']
   }
 ];
-//loop to display "wow thats big" if height is above 1.0"
-for (let i=0; i< pokemonList.length; i++)
-{
-  if (pokemonList[i].height >1.0)
-    {
-      document.write(pokemonList[i].name + pokemonList[i].height + ' Wow, that\'s big!');
-    }
-      else {
-        document.write(pokemonList[i].name + pokemonList[i].height);
-      }
-};
+//
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + '<br>' + pokemon.height + '<br>' + pokemon.types + '<br>');
+});
