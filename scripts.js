b512a1384a951744b173d644241ead474e7d8c1a
Emmanuelnz <emmanuelnun75@gmail.com>
2022-05-17 02:15:54 -0400

    created new variable for IIFE and assigned IIFE

--- scripts.js
+++ scripts.js
@@ -20,3 +20,35 @@ let pokemonList = [
 pokemonList.forEach(function(pokemon) {
   document.write(pokemon.name + '<br>' + pokemon.height + '<br>' + pokemon.types + '<br>');
 });
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Umbreon',
+      height: 1,
+      types: ['Dark']
+    },
+    {
+      name: 'Zekrom',
+      height: 2.9,
+      types: ['Dragon, Electric']
+    },
+    {
+      name: 'Luxray',
+      height: 1.4,
+      types: ['Electric']
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
