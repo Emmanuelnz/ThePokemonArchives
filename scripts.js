6768e4fa53d33c8b6086b9b0f9fd59e6d4fd6fa5
Emmanuelnz <emmanuelnun75@gmail.com>
2022-05-18 02:20:15 -0400

    removed old functions and implemented new one

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-let pokemonList = [
-  {
-    name: 'Umbreon',
-    height: 1,
-    types: ['Dark']
-  },
-  {
-    name: 'Zekrom',
-    height: 2.9,
-    types: ['Dragon, Electric']
-  },
-  {
-    name: 'Luxray',
-    height: 1.4,
-    types: ['Electric']
-  }
-];
-//
-
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + '<br>' + pokemon.height + '<br>' + pokemon.types + '<br>');
-});
 
 let pokemonRepository = (function () {
   let pokemonList = [
@@ -52,3 +30,8 @@ let pokemonRepository = (function () {
     getAll: getAll
   };
 })();
+
+pokemonRepository.getAll().forEach(function(pokemon)
+{
+pokemonRepository.add(pokemon)
+});
