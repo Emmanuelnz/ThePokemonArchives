1fbd6fde0ab40957d69b03e9c98df245b0665004
Emmanuelnz <emmanuelnun75@gmail.com>
2022-06-01 05:12:21 -0400

    added modal functions and updated old function
    The function i updated was the "ShowDetails" function. I updated it so it would show the modal of the Pokémon and not console log.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,6 +2,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -20,13 +21,14 @@ let pokemonRepository = (function () {
     button.addEventListener('click', function(){
       console.log(showDetails(pokemon));
     });
+
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
   }
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function() {
-      console.log(pokemon);
+      showModal(pokemon);
     });
   }
 
@@ -54,8 +56,67 @@ let pokemonRepository = (function () {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
+      let types = [];
+      details.types.forEach((pokemon) => types.push(pokemon.type.name));
+      item.types = types;
     });
   }
+
+  function showModal(pokemon) {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal());
+
+    let titleElement = document.createElement('h1');
+    titleElement.classList.add('pokemon-title');
+    titleElement.innerText = pokemon.name.toUpperCase();
+
+    let heightElement = document.createElement('p');
+    heightElement.classList.add('pokemon-height');
+    heightElement.innerText = 'Height: ' + pokemon.height;
+
+    let typesElement = document.createElement('p');
+    typesElement.classList.add('pokemon-types')
+    typesElement.innerText = 'Types: ' + pokemon.types;
+
+    let imgElement = document.createElement('img');
+    imgElement.src = pokemon.imageUrl;
+    imgElement.classList.add('pokemon-img');
+    imgElement.setAttribute('alt', '' + pokemon.name);
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(imgElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible')
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   return {
     add: add,
